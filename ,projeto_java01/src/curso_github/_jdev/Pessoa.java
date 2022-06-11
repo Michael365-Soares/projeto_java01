03a0ac6218ec16187b345c4d8150535edcd02d5f
micha <Michael365-Soares>
2022-06-11 16:26:05 -0300

    Criando pasta para teste de upload de arquivos github.

---
+++ ,projeto_java01/src/curso_github/_jdev/Pessoa.java
@@ -0,0 +1,31 @@
+package curso_github._jdev;
+
+public class Pessoa {
+    private String nome;
+    private String sobreNome;
+    
+    public Pessoa() {}
+    
+	public Pessoa(String nome, String sobreNome) {
+		super();
+		this.nome = nome;
+		this.sobreNome = sobreNome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobreNome() {
+		return sobreNome;
+	}
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+    
+    
+    
+    
+}
